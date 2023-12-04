e115799b0740a6593920bdcd2dce11c8bb9e5607
jaeshim <jaeshim@gmarket.com>
2023-12-04 16:30:22 +0900

    init

---
+++ java.algorithm.problem.solving.it.big/src/main/java/inflearn/java/algorithm/problem/solving/it/big/Application.java
@@ -0,0 +1,13 @@
+package inflearn.java.algorithm.problem.solving.it.big;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
